98d8647b78666d4a2156829eed96f3d1a35b2fb2
neveesx <svnzzinx@gmail.com>
2024-03-04 01:48:07 -0300

    feat(schema): adding the blacklist field

--- src/database/schema/user.ts
+++ src/database/schema/user.ts
@@ -2,8 +2,12 @@ import { Schema, model } from "mongoose";
 
 export const userObject = {
     _id: { type: String, required: true },
-    stardust: { type: Number, default: 0 }
-  }
+    stardust: { type: Number, default: 0 },
+    blacklist: {
+        banned: { type: String, default: '0' },
+        timestamp: { type: Date,  default: +Date.now()}
+    }
+};
 
 export const userSchema = new Schema(userObject);
 export const userModel = model('LunaUsers', userSchema);
