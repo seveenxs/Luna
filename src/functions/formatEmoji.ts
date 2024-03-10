88fa09a2822a0e14804aea0b349eaeff06402a75
neveesx <svnzzinx@gmail.com>
2024-03-10 13:03:53 -0300

    feat: Format keywords for Discord emojis

--- src/functions/formatEmoji.ts
+++ src/functions/formatEmoji.ts
@@ -2,8 +2,8 @@ import { LunaGlobal } from '@constants/emojis.json';
 
 export default function formatEmoji(text: string) {
     const emotesData = Object.entries(LunaGlobal);
-    const emoteRegex = new RegExp(/\[(.*?)\]/, 'g');
+    const emoteRegex = new RegExp(/#(\w+)/, 'g');
 
     return text
-    .replace(emoteRegex, (match, keyword) => emotesData.find(([name, value]) => keyword === name)?.[1] || keyword);
+    .replace(emoteRegex, (_, keyword) => emotesData.find(([name, _]) => keyword === name)?.[1] || keyword);
 };
